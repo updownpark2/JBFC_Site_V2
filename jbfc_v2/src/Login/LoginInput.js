4cc2a7fd29af1cdd9b0e7d964f0260180af3618e
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-08-22 13:55:30 +0900

    feat: User의 Input에 대한 타당성 검증을 하는 함수

--- jbfc_v2/src/Login/LoginInput.js
+++ jbfc_v2/src/Login/LoginInput.js
@@ -2,8 +2,8 @@ export default function LoginInput({ onSubmit }) {
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input placeholder="ID" />
-        <input placeholder="PW" />
+        <input placeholder="ID" maxLength="10" />
+        <input placeholder="PW" maxLength="15" />
         <button>제출</button>
       </form>
     </div>
