09f0b7c2b8e4433a5b84712c08452b3df86ebcba
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-08-22 16:28:05 +0900

    feat: 비밀번호의 가시성(visibility)를 토글하는 버튼 생성

--- jbfc_v2/src/Login/LoginInput.js
+++ jbfc_v2/src/Login/LoginInput.js
@@ -1,11 +1,34 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function LoginInput({ onSubmit }) {
+  const [passwordVisible, setPasswordVisible] = useState(true);
+
+  const togglePasswordVisibility = (event) => {
+    event.preventDefault();
+    setPasswordVisible((current) => !current);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input placeholder="ID" maxLength="10" />
-        <input placeholder="PW" maxLength="15" />
+        <input
+          placeholder="PW"
+          maxLength="15"
+          type={passwordVisible ? "password" : null}
+        />
+        <button
+          onClick={togglePasswordVisibility}
+          style={{ backgroundColor: "white", border: "0" }}
+        >
+          👁
+        </button>
+        <br />
         <button>제출</button>
       </form>
+
+      <Link to={`/signUp`}>회원이 아니신가요?</Link>
     </div>
   );
 }
