d65fb91eeaef83cc58f5e4a3d4c86cc089069d22
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-08-23 12:34:33 +0900

    Rename: 함수명 수정

--- jbfc_v2/src/Login/LoginHome.js
+++ jbfc_v2/src/Login/LoginHome.js
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
-import LoginCheckValid from "./LoginCheckValid.js";
 import LoginInput from "./LoginInput.js";
+import checkInputValid from "../CheckValid/checkInputValid.js";
 
 export default function LoginHome() {
   // URL간 이동을 위해 React Hook 사용
@@ -14,15 +14,17 @@ export default function LoginHome() {
     const ID = event.currentTarget[0].value;
     const PW = event.currentTarget[1].value;
     // ID와 PW의 타당성을 검증함
-    const logincheckvaild = new LoginCheckValid(ID, PW);
+    const checkvalid = new checkInputValid(null, ID, PW);
+
     // try catch를 여기서 해야할듯
     try {
-      logincheckvaild.checkId();
-      logincheckvaild.checkPw();
+      checkvalid.checkId();
+      checkvalid.checkPw();
       //통과하면 여기서부터 실행됨 => 서버에 ID PW넘겨주고 일치불일치를 찾아야함
       //
       goToHome();
       //여기서 DB에 있는 정보인지를 판단함
+      //판단하고 만약 play type이 정해져있지 않다면 설문 페이지를 rendering
     } catch (error) {
       // error가 존재한다면 alert로 메세지를 보냄
       alert(error.message);
