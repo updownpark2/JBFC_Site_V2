8299339d884873db96f6e66da295fc2bd5df70c8
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-09-12 17:11:01 +0900

    feat: 좋아요, 싫어요 버튼을 누르면 DB에 각 유저를 구분해서 저장할 수 있는 기능 구현
    userId당 한 개의 좋아요와 1개의 싫어요만 사용이 가능하도록 개발
    DB에 arr형식으로 push하여 그 길이로 좋아요 수를 알 수 있도록 함

--- jbfc_v2/src/Board/BoardDetailModel.js
+++ jbfc_v2/src/Board/BoardDetailModel.js
@@ -1,8 +1,43 @@
 import axios from "axios";
 
-export default async function getBoardDetail(boardId) {
+export async function getBoardDetail(boardId) {
   let boardDetail = await axios.post(`http://localhost:8080/getBoardDetail`, {
     boardId: boardId,
   });
   return boardDetail;
 }
+// 이건 boardDetail 정보를 가져오는 함수
+
+export async function likeThisPush(boardId, userId) {
+  const boardDetail = await getBoardDetail(boardId);
+  if (!boardDetail.data.likeThis.includes(userId)) {
+    axios.post(`http://localhost:8080/pushLikeThis`, {
+      boardId: boardId,
+      userId: userId,
+    });
+  }
+  //만약 포함되어있다면 이제 삭제
+  else {
+    axios.post(`http://localhost:8080/popLikeThis`, {
+      boardId: boardId,
+      userId: userId,
+    });
+  }
+}
+
+export async function hateThisPush(boardId, userId) {
+  const boardDetail = await getBoardDetail(boardId);
+  if (!boardDetail.data.hateThis.includes(userId)) {
+    axios.post(`http://localhost:8080/pushHateThis`, {
+      boardId: boardId,
+      userId: userId,
+    });
+  } else {
+    axios.post(`http://localhost:8080/popHateThis`, {
+      boardId: boardId,
+      userId: userId,
+    });
+  }
+}
+
+//이제 좋아요를 받아서 수정하는 작업
--- jbfc_v2/src/Board/BoardDetailOutput.js
+++ jbfc_v2/src/Board/BoardDetailOutput.js
@@ -1,16 +1,24 @@
 import { useLocation } from "react-router-dom";
-import getBoardDetail from "./BoardDetailModel";
+import { getBoardDetail, hateThisPush, likeThisPush } from "./BoardDetailModel";
 import { useState } from "react";
 import { useEffect } from "react";
 export default function BoardDetailOutput() {
-  const boardId = useLocation().state;
+  const { boardId } = useLocation().state;
   const [boardDetail, setBoardDetail] = useState([]);
+  const [likeThis, setLikeThis] = useState([]);
+  const [hateThis, setHateThis] = useState([]);
+  // state에 저장
 
   const setBoardDetailData = async (boardId) => {
     const boardDetail = await getBoardDetail(boardId);
 
     setBoardDetail(boardDetail.data);
-  };
+    setLikeThis(boardDetail.data.likeThis.length);
+    setHateThis(boardDetail.data.hateThis.length);
+    //처음에 한번 불러오고
+  }; // set한다 BoardData를
+
+  //이거하면 다시 수정해야하는데
 
   useEffect(() => {
     const getAndsetBoardDetailApi = async () => {
@@ -31,10 +39,23 @@ export default function BoardDetailOutput() {
             {boardDetail.month}월 {boardDetail.day}
           </span>
           <p>작성자: {boardDetail.userId}</p>
-          <span>좋아요 싫어요</span>
+          <span>
+            {likeThis}
+            {hateThis}
+          </span>
           <br />
-          <button type="button">👍</button>
-          <button type="button">👎</button>
+          <button
+            onClick={() => likeThisPush(boardId, boardDetail.userId)}
+            type="button"
+          >
+            👍
+          </button>
+          <button
+            onClick={() => hateThisPush(boardId, boardDetail.userId)}
+            type="button"
+          >
+            👎
+          </button>
         </div>
       )}
     </div>
